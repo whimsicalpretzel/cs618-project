5196291b28941fe6be60e35a9993694a86731b5f
Lance <156000490+whimsicalpretzel@users.noreply.github.com>
2025-10-30 18:06:36 +0000

    chore: video 3.5 setup TanStack Query

---
+++ src/Blog.jsx
@@ -0,0 +1,29 @@
+import { PostList } from './components/PostList.jsx'
+import { CreatePost } from './components/CreatePost.jsx'
+import { PostFilter } from './components/PostFilter.jsx'
+import { PostSorting } from './components/PostSorting.jsx'
+
+const posts = [
+  {
+    title: 'Full-Stack React Projects',
+    contents: "Let's become full-stack developers!",
+    author: 'Daniel Bugl',
+  },
+  { title: 'Hello React!' },
+]
+
+export function Blog() {
+  return (
+    <div style={{ padding: 8 }}>
+      <CreatePost />
+      <br />
+      <hr />
+      Filter by:
+      <PostFilter field='author' />
+      <br />
+      <PostSorting fields={['createdAt', 'updatedAt']} />
+      <hr />
+      <PostList posts={posts} />
+    </div>
+  )
+}
