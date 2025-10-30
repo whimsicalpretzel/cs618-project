5021e9941478a2fa279e92877170a7f17c5f0469
Lance <156000490+whimsicalpretzel@users.noreply.github.com>
2025-10-30 18:17:54 +0000

    chore: video 3.6 added ability to fetch data from datebase

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -2,17 +2,16 @@ import { PostList } from './components/PostList.jsx'
 import { CreatePost } from './components/CreatePost.jsx'
 import { PostFilter } from './components/PostFilter.jsx'
 import { PostSorting } from './components/PostSorting.jsx'
-
-const posts = [
-  {
-    title: 'Full-Stack React Projects',
-    contents: "Let's become full-stack developers!",
-    author: 'Daniel Bugl',
-  },
-  { title: 'Hello React!' },
-]
+import { useQuery } from '@tanstack/react-query'
+import { getPosts } from './api/posts.js'
 
 export function Blog() {
+  const postsQuery = useQuery({
+    queryKey: ['posts'],
+    queryFn: () => getPosts(),
+  })
+  const posts = postsQuery.data ?? []
+
   return (
     <div style={{ padding: 8 }}>
       <CreatePost />
---
+++ src/api/posts.js
@@ -0,0 +1,7 @@
+export const getPosts = async (queryParams) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_BACKEND_URL}/posts?` +
+      new URLSearchParams(queryParams),
+  )
+  return await res.json()
+}
