8e2227e61da737ea04cc4f90bb1a2d37fbe3fd00
Lance <156000490+whimsicalpretzel@users.noreply.github.com>
2025-10-30 19:02:20 +0000

    chore: video 4.8 creating new posts and invalidating queries

--- src/api/posts.js
+++ src/api/posts.js
@@ -5,3 +5,16 @@ export const getPosts = async (queryParams) => {
   )
   return await res.json()
 }
+
+export const createPost = async (post) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_BACKEND_URL}/
+posts`,
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(post),
+    },
+  )
+  return await res.json()
+}
