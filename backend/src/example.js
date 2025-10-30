b995623ded6485cb8b05df8fffaee862f364f9fc
Lance <156000490+whimsicalpretzel@users.noreply.github.com>
2025-10-30 18:45:40 +0000

    feat: video 3.7 added filters and sorting

--- backend/src/example.js
+++ backend/src/example.js
@@ -1,20 +1,19 @@
 import { initDatabase } from './db/init.js'
 import { Post } from './db/models/post.js'
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 await initDatabase()
+
 const post = new Post({
-  title: 'Hello Mongoose!',
-  author: 'Lance Perkins',
-  contents: 'This post is stored in a MongoDB database using Mongoose.',
-  tags: ['mongoose', 'mongodb'],
+  title: 'Hello second post!',
+  author: 'John Doe',
+  contents: 'This is my new exciting content',
+  tags: ['frontend'],
 })
 
-const createdPost = await post.save()
-
-await Post.findByIdAndUpdate(createdPost._id, {
-  $set: { title: 'Hello again, Mongoose!' },
-})
-
+await post.save()
 const posts = await Post.find()
 
 console.log(posts)
