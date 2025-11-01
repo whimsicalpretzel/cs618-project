4372c8fafa301e540f1c7578f7b8a50d415845cb
Lance <156000490+whimsicalpretzel@users.noreply.github.com>
2025-11-01 18:36:34 +0000

    chore: videos 5.2 and 5.3 created docker containers for FE and BE

--- src/api/posts.js
+++ src/api/posts.js
@@ -7,14 +7,10 @@ export const getPosts = async (queryParams) => {
 }
 
 export const createPost = async (post) => {
-  const res = await fetch(
-    `${import.meta.env.VITE_BACKEND_URL}/
-posts`,
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(post),
-    },
-  )
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(post),
+  })
   return await res.json()
 }
