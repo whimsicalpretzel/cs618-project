16fbd9841eea03ed8f26a576be7318166b5b1556
Lance <156000490+whimsicalpretzel@users.noreply.github.com>
2025-11-02 00:51:48 +0000

    feat: added authentication, routing, and updated tests

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,10 @@
-import { PostList } from './components/PostList.jsx'
-import { CreatePost } from './components/CreatePost.jsx'
-import { PostFilter } from './components/PostFilter.jsx'
-import { PostSorting } from './components/PostSorting.jsx'
+import { PostList } from '../components/PostList.jsx'
+import { CreatePost } from '../components/CreatePost.jsx'
+import { PostFilter } from '../components/PostFilter.jsx'
+import { PostSorting } from '../components/PostSorting.jsx'
+import { Header } from '../components/Header.jsx'
 import { useQuery } from '@tanstack/react-query'
-import { getPosts } from './api/posts.js'
+import { getPosts } from '../api/posts.js'
 import { useState } from 'react'
 
 export function Blog() {
@@ -19,7 +20,9 @@ export function Blog() {
 
   return (
     <div style={{ padding: 8 }}>
-      <h1>Welcome to My Blog!</h1>
+      <Header />
+      <hr />
+      <br />
       <CreatePost />
       <br />
       <hr />
