cb971849d26f7c2d1c640271c6daf03213dbde2f
Lance <156000490+whimsicalpretzel@users.noreply.github.com>
2025-11-08 22:00:18 +0000

    feat: added statistics for view counts and stat visualization with Victory

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,14 @@ import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import { eventRoutes } from './routes/events.js'
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 postsRoutes(app)
 userRoutes(app)
+eventRoutes(app)
 
 app.get('/', (req, res) => {
   res.send('Hello from Express Nodemon!')
